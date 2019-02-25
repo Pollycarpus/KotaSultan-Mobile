9ca301f83726d5d02021e7647f628407f6ca00dc
Rahmat Santosa <rnsantosa@Rahmats-MacBook-Pro.local>
2019-02-25 17:52:16 +0700

    Add shake to win

--- app/src/main/java/com/example/kotasultan/ShakeToWin.java
+++ app/src/main/java/com/example/kotasultan/ShakeToWin.java
@@ -1,11 +1,25 @@
 package com.example.kotasultan;
 
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 
 /**
@@ -13,6 +27,9 @@ import android.view.ViewGroup;
  */
 public class ShakeToWin extends Fragment {
 
+    private SensorManager mSensorManager;
+
+    private ShakeEventListener mSensorListener;
 
     public ShakeToWin() {
         // Required empty public constructor
@@ -22,8 +39,64 @@ public class ShakeToWin extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View view = inflater.inflate(R.layout.fragment_shake_to_win, container, false);
+
+        final ImageView mCoinImage = (ImageView) view.findViewById(R.id.coinImage);
+        final TextView mCoinText = (TextView) view.findViewById(R.id.coinText);
+
+        mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mSensorListener = new ShakeEventListener();
+
+        mSensorListener.setOnShakeListener(new ShakeEventListener.OnShakeListener() {
+
+            public void onShake() {
+                int random = new Random().nextInt(6);
+                int coinValue = 20 * random;
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+                builder1.setMessage("You got " + coinValue + " coins!");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+
+                if (!ShakeEventListener.getShakeAvailable()) {
+                    mCoinText.setText("Shake again tomorrow!");
+                    mCoinImage.setImageResource(R.drawable.business_inactive);
+                    Log.d("GetShakeAvailable", "FALSE");
+                } else {
+                    mCoinText.setText("Shake your phone!");
+                    mCoinImage.setImageResource(R.drawable.business);
+                    Log.d("GetShakeAvailable", "TRUE");
+                }
+            }
+
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shake_to_win, container, false);
+        return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mSensorListener,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
+    }
 }
