cd49495914f9db122d337953c0ce624eefb1129b
Pollycarpus <fijarmayoza.rabbi@gmail.com>
2019-02-25 14:00:09 +0700

    add location service

--- app/src/main/java/com/example/kotasultan/Catatanku.java
+++ app/src/main/java/com/example/kotasultan/Catatanku.java
@@ -1,7 +1,12 @@
 package com.example.kotasultan;
 
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,6 +15,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Comparator;
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 
 /**
@@ -29,9 +44,25 @@ public class Catatanku extends Fragment {
     private  EditText editValuePemasukan;
     private  EditText editDatePemasukan;
 
+    public static SortedMap<Currency, Locale> currencyLocaleMap;
+    TextView t;
+    Geocoder geocoder;
+    private static final Map< String, Locale > COUNTRY_TO_LOCALE_MAP = new HashMap< String, Locale >();
+    static {
+        Locale[] locales = Locale.getAvailableLocales();
+        for (Locale l: locales) {
+            COUNTRY_TO_LOCALE_MAP.put(l.getCountry(), l);
+        }
+    }
+    public static Locale getLocaleFromCountry(String country) {
+        return COUNTRY_TO_LOCALE_MAP.get(country);
+    }
+
+    String Currencysymbol = "";
+
+
     public Catatanku() {
         // Required empty public constructor
-        Log.d("TES", "Ada");
 
     }
 
@@ -41,6 +72,102 @@ public class Catatanku extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_catatanku, container, false);
+
+        Button buttonCurrency = (Button) view.findViewById(R.id.button_currency);
+        buttonCurrency.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                TextView t1 = (TextView) getView().findViewById(R.id.currency);
+                TextView t2 = (TextView) getView().findViewById(R.id.currency2);
+
+                GPSTracker gpsTracker = new GPSTracker(getActivity());
+                geocoder = new Geocoder(getActivity(), getLocaleFromCountry(""));
+                LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+                boolean gps_enabled = false;
+                boolean network_enabled = false;
+                gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                if(!gps_enabled && !network_enabled){
+                new AlertDialog.Builder(getActivity())
+                            .setTitle("GPS Not Found!").show();   ;  // GPS not found
+                } else {
+                    double lat_calibrate = -6.90389;
+                    double lng_calibrate = 107.61861;
+                    double lat = gpsTracker.getLatitude() + lat_calibrate;
+                    double lng = gpsTracker.getLongitude() + lng_calibrate;
+
+
+                    Log.e("Lat long ", lng + "lat long check" + lat);
+                    Log.d("Lat long ", lng + "lat long check" + lat);
+
+                    currencyLocaleMap = new TreeMap<Currency, Locale>(new Comparator<Currency>() {
+                        public int compare(Currency c1, Currency c2) {
+                            return c1.getCurrencyCode().compareTo(c2.getCurrencyCode());
+                        }
+                    });
+
+                    for (Locale locale : Locale.getAvailableLocales()) {
+                        try {
+                            Currency currency = Currency.getInstance(locale);
+                            currencyLocaleMap.put(currency, locale);
+
+                            Log.d("locale util", currency + " locale1 " + locale.getCountry());
+
+                        } catch (Exception e) {
+
+                            Log.d("locale util", "e" + e);
+                        }
+                    }
+
+                    try {
+
+                        List<Address> addresses = geocoder.getFromLocation(lat, lng, 2);
+                        Address obj = addresses.get(0);
+                        Currencysymbol = getCurrencyCode(obj.getCountryCode());
+
+                        Log.e("getCountryCode", "Exception address " + obj.getCountryCode());
+                        Log.e("Currencysymbol", "Exception address " + Currencysymbol);
+
+                    } catch (Exception e) {
+                        Log.e("Exception address", "Exception address" + e);
+                        Log.e("Currencysymbol", "Exception address" + Currencysymbol);
+                    }
+                    Log.d("Ada", Currencysymbol);
+                }
+                if(Currencysymbol != null) {
+                    t1.setText(Currencysymbol);
+                    t2.setText(Currencysymbol);
+                }
+            }
+
+            public String getCurrencyCode(String countryCode) {
+
+                String s = "";
+                for (Locale locale: Locale.getAvailableLocales()) {
+                    try {
+
+                        if (locale.getCountry().equals(countryCode)) {
+
+                            Currency currency = Currency.getInstance(locale);
+                            currencyLocaleMap.put(currency, locale);
+                            Log.d("locale util", currency + " locale1 " + locale.getCountry() + "s " + s);
+                            s = getCurrencySymbol(currency + "");
+                        }
+                    } catch (Exception e) {
+                        Log.d("locale util", "e" + e);
+                    }
+                }
+                return s;
+            }
+
+            public String getCurrencySymbol(String currencyCode) {
+                Currency currency = Currency.getInstance(currencyCode);
+                System.out.println(currencyCode + ":-" + currency.getSymbol(currencyLocaleMap.get(currency)));
+                return currency.getSymbol(currencyLocaleMap.get(currency));
+            }
+        });
 
         Button buttonPengeluaran = (Button) view.findViewById(R.id.button_pengeluaran);
         buttonPengeluaran.setOnClickListener(new View.OnClickListener()
